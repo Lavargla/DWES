1fd33d71f6cb16fd2ebcd994d782a96776e9fdde
Lavargla <102350428+Lavargla@users.noreply.github.com>
2022-10-25 20:43:41 +0200

    Add files via upload

---
+++ Ej_PHP_DWES/UT01/Ej3_UT01.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+      echo "<table>";
+      echo  '<tr><th>Spanish</th><th>English</th></tr>';
+      echo  '<tr><td>caca</td><td>shit</td></tr>';
+      echo  '<tr><td>esquina</td><td>corner</td></tr>';
+      echo  '<tr><td>nosotros</td><td>us</td></tr>';
+      echo  '<tr><td>beber</td><td>drink</td></tr>';
+      echo  '<tr><td>hielo</td><td>ice</td></tr>';
+      echo  '<tr><td>veneno</td><td>poison</td></tr>';
+      echo  '<tr><td>ordenador</td><td>pc</td></tr>';
+      echo  '<tr><td>monstruo</td><td>monster</td></tr>';
+      echo  '<tr><td>pregunta</td><td>question</td></tr>';
+      echo "</table>";
+    ?>
+</body>
+</html>
---
+++ Ej_PHP_DWES/UT01/Ej4_UT01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+    <tr><th>Lunes</th><th>Martes</th><th>Miercoles</th><th>Jueves</th><th>Viernes</th></tr>
+    <?php
+        for($i = 1; $i <= 6; $i++){
+            echo '<tr><td>among us</td><td>among us</td><td>among us</td><td>among us</td><td>among us</td></tr>';
+        }
+    ?>
+    </table>
+</body>
+</html>
